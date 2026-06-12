523c1f210dce29eb626aa8c2fe4ad81ac4d7252c
misaliba <misalibaytb@gmail.com>
2026-06-12 21:59:25 +0200

    g

--- dist/assets/scripts/scroll.js
+++
@@ -1,9 +0,0 @@
-const updateScrollButton = () => {
-  document.body.classList.toggle("scrolled", window.scrollY > 100);
-};
-
-updateScrollButton();
-
-window.addEventListener("scroll", updateScrollButton, {
-  passive: true,
-});
